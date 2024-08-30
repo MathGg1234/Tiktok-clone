f37fa4b7dc78ae520e1b4a6d51bb61d75ca70a45
raphavat <161726196+raphavat@users.noreply.github.com>
2024-08-30 16:27:20 +0200

    Update index.js

--- tiktok_clone/index.js
+++ tiktok_clone/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const sequelize = require('./config/database');
-const { User, Video } = require('./models');  // Importe les modèles avec les associations
 const authRoutes = require('./routes/auth');
-const videoRoutes = require('./routes/videos');
+const videoRoutes = require('./routes/videos'); // Assurez-vous que ce fichier est correctement configuré
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
@@ -11,15 +12,20 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/videos', videoRoutes);
 
+// Servir la page d'accueil
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Lire les certificats SSL
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/nutix.fun/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/nutix.fun/fullchain.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+// Synchroniser la base de données et démarrer le serveur HTTPS
 sequelize.sync().then(() => {
-    app.listen(3000, () => {
-        console.log('Server running on http://localhost:3000');
+    https.createServer(credentials, app).listen(443, () => {
+        console.log('Server running on https://nutix.fun');
     });
 });
